00f00197860b4efdd1627f683f8450089d25dabb
CKamande <mungagpi@gmail.com>
2025-04-14 17:12:09 +0000

    DONE

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,36 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self._name = None
+        self._job = None
+        
+        # Only set name if provided, otherwise skip validation
+        if name is not None:
+            self.name = name  # Uses name setter
+        
+        # Only set job if provided, otherwise skip validation
+        if job is not None:
+            self.job = job    # Uses job setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
